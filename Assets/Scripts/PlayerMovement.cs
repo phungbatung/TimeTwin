b2b16c83d2a1fe2d0369366149bd8183fd540d59
phungbatung <phungbatung2000na@gmail.com>
2025-01-05 21:08:45 +0700

    Player movement

---
+++ Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,65 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.InputSystem;
+public class PlayerMovement : MonoBehaviour
+{
+    private float horizontal = 0;
+    public bool facingRight { get; private set; }
+    [SerializeField] private float jumpForce;
+    [SerializeField] private float moveSpeed;
+    [SerializeField] private Transform groundCheck;
+    [SerializeField] private float groundCheckDistance;
+    [SerializeField] LayerMask groundLayer;
+
+    private Rigidbody2D rb;
+    private Vector3 rootPosition;
+    private void Awake()
+    {
+        rb=GetComponent<Rigidbody2D>();
+        facingRight = true;
+        rootPosition = transform.position;
+    }
+
+    public void Update()
+    {
+        rb.velocity = new Vector2(horizontal * moveSpeed, rb.velocity.y);
+        FlipCheck();
+    }
+    public void Flip()
+    {
+        facingRight = !facingRight;
+        transform.Rotate(new Vector3(0, 180, 0));
+    }
+    public void FlipCheck()
+    {
+        if( (horizontal<0 && facingRight) || (horizontal>0 && !facingRight))
+            Flip();
+    }
+    public void Jump(InputAction.CallbackContext context)
+    {
+        if(context.started&& IsGrounded())
+        {
+            rb.velocity = new Vector2 (rb.velocity.x, jumpForce);
+        }
+    }
+    public void GetHorizontal(InputAction.CallbackContext context)
+    {
+        if (context.performed)
+        {
+            horizontal = context.ReadValue<float>();
+        }
+        else if (context.canceled)
+        {
+            horizontal = 0;
+        }
+    }
+    public bool IsGrounded()
+    {
+        return Physics2D.Raycast(groundCheck.position, Vector2.down, groundCheckDistance, groundLayer);
+    }
+    public void ResetPostion()
+    {
+        transform.position = rootPosition;
+    }
+}
